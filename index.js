ebb2efea84f68b7d1ec4cf10df83e88b5c8774d0
nuclearFoxCat-1160 <patsy.lo.312@gmail.com>
2026-05-17 14:56:27 +0100

    Add counter files to upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+let resetEl = document.getElementById("reset-btn")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+function reset() {
+    saveEl.textContent = "Previous entries: "
+        countEl.textContent = 0
+    count = 0
+}
